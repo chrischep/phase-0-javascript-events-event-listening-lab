0e9178fbcfbd0bd0333c2a4af8c0e11fb9526629
chrischep <christine.chepleting@student.moringaschool.com>
2022-08-29 09:35:30 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById("input");
+console.log(input);
 function addingEventListener() {
+	input.addEventListener("click", addingEventListener);
 }
+
+
+
+// function addingEventListener() {
+// }
